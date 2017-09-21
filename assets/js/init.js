d891709e6018d31204b8d3a055a5decd3958475c
Rommel7 <ramil.mamedov7991@gmail.com>
2017-09-21 13:15:43 +0400

    mix it up plugin error

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,74 +1,58 @@
-$(document).ready(function() {
+$(document).ready(function () {
     "use strict";
 
-    /***************************************************************************/
-            /* NAVIGATION  */
-    /***************************************************************************/
-   
-      $('.button-collapse').sideNav();
- 
-    /**************************************************************************
-                 SKILL BAR 
-    **************************************************************************/
+    $('.button-collapse').sideNav();
 
-    $(".determinate").each(function(){
-      var width = $(this).text();
-      $(this).css("width", width)
-        .empty()
-        .append('<i class="fa fa-circle"></i>');                
+
+    $(".determinate").each(function () {
+        var width = $(this).text();
+        $(this).css("width", width)
+            .empty()
+            .append('<i class="fa fa-circle"></i>');
     });
 
 
-    /**************************************************************************
-            Style demo
-    **************************************************************************/
-   
-    $('.cv-style-switch').click(function(){
-        if($(this).hasClass('open')){
-          $(this).removeClass('open');
-          $('#switch-style').animate({'right':'0'});
-        }else{
-          $(this).addClass('open');
-          $('#switch-style').animate({'right':'-300'});
+    $('.cv-style-switch').click(function () {
+        if ($(this).hasClass('open')) {
+            $(this).removeClass('open');
+            $('#switch-style').animate({
+                'right': '0'
+            });
+        } else {
+            $(this).addClass('open');
+            $('#switch-style').animate({
+                'right': '-300'
+            });
         }
     });
 
-  
-    /**************************************************************************
-             BLOG POST 
-    **************************************************************************/
-  
-    jQuery(window).on('load', function(){ var $ = jQuery;
+
+    jQuery(window).on('load', function () {
+        var $ = jQuery;
         $('.blog').masonry({
-          itemSelector: '.blog-post',
-          columnWidth: '.blog-post',
-          percentPosition: true
+            itemSelector: '.blog-post',
+            columnWidth: '.blog-post',
+            percentPosition: true
         });
     });
 
 
     var height = $('.caption').height();
-        if($(window).width()){
-          $('#featured').css('height', height);   
-          $('#featured img').css('height', height);   
-        }
+    if ($(window).width()) {
+        $('#featured').css('height', height);
+        $('#featured img').css('height', height);
+    }
 
+    $('.tooltipped').tooltip({
+        delay: 50
+    });
 
-    /*************************************************************************
-                TOOLTIP
-    **************************************************************************/
-    $('.tooltipped').tooltip({delay: 50});
-
-    /**************************************************************************
-        WOW INIT
-    **************************************************************************/
-
-    var wow = new WOW({ mobile: false });
+    var wow = new WOW({
+        mobile: false
+    });
     wow.init();
 
-    /***************************************************************************
-          CONTACT FORM
-    ***************************************************************************/
+    /*** CONTACT FORM **/
 
     $("#contactForm").validator().on("submit", function (event) {
         if (event.isDefaultPrevented()) {
@@ -81,96 +65,121 @@ $(document).ready(function() {
             submitForm();
         }
     });
-    function submitForm(){
-    // Initiate Variables With Form Content
+
+    function submitForm() {
+        // Initiate Variables With Form Content
         var name = $("#name").val();
         var email = $("#email").val();
         var message = $("#message").val();
-        
+
         $.ajax({
             type: "POST",
             url: "process.php",
             data: "name=" + name + "&email=" + email + "&message=" + message,
-            success : function(text){
-                if (text == "success"){
+            success: function (text) {
+                if (text == "success") {
                     formSuccess();
                 } else {
                     formError();
-                    submitMSG(false,text);
+                    submitMSG(false, text);
                 }
             }
         });
     }
-    function formSuccess(){
+
+    function formSuccess() {
         $("#contactForm")[0].reset();
         submitMSG(true, "Message Sent!")
     }
-    function formError(){
-        $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', 
-        function(){
-            $(this).removeClass();
-        });
+
+    function formError() {
+        $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
+            function () {
+                $(this).removeClass();
+            });
     }
-    function submitMSG(valid, msg){
-        if(valid){
+
+    function submitMSG(valid, msg) {
+        if (valid) {
             var msgClasses = "h3 text-center fadeInUp animated text-success";
         } else {
             var msgClasses = "h3 text-center text-danger";
         }
         $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
     }
-    
-    	
-    /**************************************************************************
-       Projects
-    **************************************************************************/
-    $('#portfolio-item').mixItUp();
-    
-    $('.sa-view-project-detail').on('click', function(event) {
-        event.preventDefault();
-        var href          = $(this).attr('href') + ' ' + $(this).attr('data-action'),
-            dataShow      = $('#project-gallery-view'),
-            dataShowMeta  = $('#project-gallery-view meta'),
-            dataHide      = $('#portfolio-item'),
-            preLoader     = $('#loader'),
-            backBtn       = $('#back-button'),
-            filterBtn     = $('#filter-button');
 
-        dataHide.animate( { 'marginLeft':'-120%' }, { duration: 400, queue: false } );
-        filterBtn.animate( { 'marginLeft':'-120%' }, { duration: 400, queue: false } );
+
+    /*** Projects ***/
+    
+    //    $('#portfolio-item').mixItUp();
+
+    $('.sa-view-project-detail').on('click', function (event) {
+        event.preventDefault();
+        var href = $(this).attr('href') + ' ' + $(this).attr('data-action'),
+            dataShow = $('#project-gallery-view'),
+            dataShowMeta = $('#project-gallery-view meta'),
+            dataHide = $('#portfolio-item'),
+            preLoader = $('#loader'),
+            backBtn = $('#back-button'),
+            filterBtn = $('#filter-button');
+
+        dataHide.animate({
+            'marginLeft': '-120%'
+        }, {
+            duration: 400,
+            queue: false
+        });
+        filterBtn.animate({
+            'marginLeft': '-120%'
+        }, {
+            duration: 400,
+            queue: false
+        });
         dataHide.fadeOut(400);
         filterBtn.fadeOut(400);
-        setTimeout( function() { preLoader.show(); }, 400);
-        setTimeout( function() {
-            dataShow.load( href, function() {
+        setTimeout(function () {
+            preLoader.show();
+        }, 400);
+        setTimeout(function () {
+            dataShow.load(href, function () {
                 dataShowMeta.remove();
                 preLoader.hide();
                 dataShow.fadeIn(600);
                 backBtn.fadeIn(600);
             });
-        },800);
+        }, 800);
     });
 
-    $('#back-button').on('click', function(event) {
+    $('#back-button').on('click', function (event) {
         event.preventDefault();
-        var dataShow    = $('#portfolio-item'),
-            dataHide    = $('#project-gallery-view'),
-            filterBtn   = $('#filter-button');
+        var dataShow = $('#portfolio-item'),
+            dataHide = $('#project-gallery-view'),
+            filterBtn = $('#filter-button');
 
-        $("[data-animate]").each( function() {
+        $("[data-animate]").each(function () {
             $(this).addClass($(this).attr('data-animate'));
         });
 
         dataHide.fadeOut(400);
         $(this).fadeOut(400);
-        setTimeout(function(){
-            dataShow.animate( { 'marginLeft': '0' }, { duration: 400, queue: false } );
-            filterBtn.animate( { 'marginLeft': '0' }, { duration: 400, queue: false } );
+        setTimeout(function () {
+            dataShow.animate({
+                'marginLeft': '0'
+            }, {
+                duration: 400,
+                queue: false
+            });
+            filterBtn.animate({
+                'marginLeft': '0'
+            }, {
+                duration: 400,
+                queue: false
+            });
             dataShow.fadeIn(400);
             filterBtn.fadeIn(400);
-        },400);
-        setTimeout(function(){
+        }, 400);
+        setTimeout(function () {
             dataShow.find('.fadeInRight, .fadeInLeft, .fadeInUp, .fadeInDown').removeClass('fadeInRight').removeClass('fadeInLeft').removeClass('fadeInUp').removeClass('fadeInDown');
-        },1500);
+        }, 1500);
     });
 });
